deca036b8541cb34e059a915a755cc834fe06df6
opaluch.lukasz <opaluch.lukasz@gmail.com>
2016-12-19 22:11:21 +0100

    kd-trees problem initial

---
+++ main/kd-trees/KdTree.java
@@ -0,0 +1,46 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+import java.util.ArrayList;
+
+public class KdTree {
+    // construct an empty set of points
+    public KdTree() {
+
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // number of points in the set
+    public int size() {
+        return 0;
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D point) {
+
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D point) {
+        return false;
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+
+    }
+
+    // all points that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) {
+        return new ArrayList<>();
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        return null;
+    }
+}
---
+++ main/kd-trees/PointSET.java
@@ -0,0 +1,47 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+import java.util.ArrayList;
+
+public class PointSET {
+
+    // construct an empty set of points
+    public PointSET() {
+
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // number of points in the set
+    public int size() {
+        return 0;
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D point) {
+
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D point) {
+        return false;
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+
+    }
+
+    // all points that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) {
+        return new ArrayList<>();
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        return null;
+    }
+}
