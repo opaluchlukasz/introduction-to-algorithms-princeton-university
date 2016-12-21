a4b6cc31dff20c6e0129145abd387a0c2a11efab
opaluch.lukasz <opaluch.lukasz@gmail.com>
2016-12-21 18:49:57 +0100

    kd-trees problem - pointSet impl

--- main/kd-trees/PointSET.java
+++ main/kd-trees/PointSET.java
@@ -1,33 +1,37 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
-import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static java.util.Comparator.comparingDouble;
+import static java.util.stream.Collectors.toList;
 
 public class PointSET {
 
-    // construct an empty set of points
-    public PointSET() {
-
-    }
+    private Set<Point2D> points = new TreeSet<>();
 
     // is the set empty?
     public boolean isEmpty() {
-        return false;
+        return points.isEmpty();
     }
 
     // number of points in the set
     public int size() {
-        return 0;
+        return points.size();
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D point) {
-
+        if (!points.contains(point)) {
+            points.add(point);
+        }
     }
 
     // does the set contain point p?
     public boolean contains(Point2D point) {
-        return false;
+        return points.contains(point);
     }
 
     // draw all points to standard draw
@@ -36,12 +40,13 @@ public class PointSET {
     }
 
     // all points that are inside the rectangle
-    public Iterable<Point2D> range(RectHV rect) {
-        return new ArrayList<>();
+    public Iterable<Point2D> range(RectHV rectangle) {
+        return points.stream().filter(rectangle::contains).collect(toList());
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
-    public Point2D nearest(Point2D p) {
-        return null;
+    public Point2D nearest(Point2D point) {
+        Optional<Point2D> nearest = points.stream().sorted(comparingDouble(o -> o.distanceTo(point))).findFirst();
+        return nearest.orElse(null);
     }
 }
--- test/kd-trees/PointSETTest.groovy
+++ test/kd-trees/PointSETTest.groovy
@@ -1,4 +1,66 @@
+import edu.princeton.cs.algs4.Point2D
+import edu.princeton.cs.algs4.RectHV
 import spock.lang.Specification
 
+import static org.hamcrest.Matchers.containsInAnyOrder
+import static spock.util.matcher.HamcrestSupport.expect
+
 class PointSETTest extends Specification {
+
+    def 'should return true for empty set'() {
+        expect:
+        new PointSET().empty
+    }
+
+    def 'should return false for non-empty set'() {
+        given:
+        PointSET pointSET = new PointSET()
+        pointSET.insert(new Point2D(1d, 1d))
+
+        expect:
+        !pointSET.empty
+    }
+
+    def 'should return points within rectangle'(List<Point2D> points, List<Point2D> expected) {
+        given:
+        PointSET pointSET = new PointSET()
+        points.forEach { pointSET.insert(it) }
+
+        when:
+        Iterable<Point2D> within = pointSET.range(new RectHV(-1d, -1d, 1d, 1d))
+
+        then:
+        expect within, containsInAnyOrder(expected.toArray())
+
+        where:
+        points                                               | expected
+        []                                                   | []
+        [new Point2D(2d, 2d)]                                | []
+        [new Point2D(-1.1d, -1.1d), new Point2D(1.1d, 1.1d)] | []
+        [new Point2D(-1d, -1d), new Point2D(-1d, 1d),
+         new Point2D(1d, -1d), new Point2D(1d, 1d)]          | [new Point2D(-1d, -1d), new Point2D(-1d, 1d),
+                                                                new Point2D(1d, -1d), new Point2D(1d, 1d)]
+        [new Point2D(-1d, 0d), new Point2D(0d, 1d),
+         new Point2D(1d, 0d), new Point2D(0d, -1d)]          | [new Point2D(-1d, 0d), new Point2D(0d, 1d),
+                                                                new Point2D(1d, 0d), new Point2D(0d, -1d)]
+    }
+
+    def 'should return nearest point'(List<Point2D> points, Point2D expected) {
+        given:
+        PointSET pointSET = new PointSET()
+        points.forEach { pointSET.insert(it) }
+
+        when:
+        Point2D nearest = pointSET.nearest(new Point2D(1d, 1d))
+
+        then:
+        nearest == expected
+
+        where:
+        points                                       | expected
+        []                                           | null
+        [new Point2D(2d, 2d)]                        | new Point2D(2d, 2d)
+        [new Point2D(1d, 1d), new Point2D(2d, 2d)]   | new Point2D(1d, 1d)
+        [new Point2D(4d, 4d), new Point2D(-1d, -1d)] | new Point2D(-1d, -1d)
+    }
 }
