b08dabf3b6ebbe7ade8ccac16bf04c6a6517f38e
opaluch.lukasz <opaluch.lukasz@gmail.com>
2016-12-29 20:21:19 +0100

    kd-trees problem - kdtree draft

--- main/kd-trees/KdTree.java
+++ main/kd-trees/KdTree.java
@@ -2,30 +2,50 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class KdTree {
-    // construct an empty set of points
-    public KdTree() {
 
-    }
+    private Node root;
+    private int size = 0;
 
     // is the set empty?
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     // number of points in the set
     public int size() {
-        return 0;
+        return size;
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D point) {
-
+        if (!contains(point)) {
+            if (root == null) {
+                root = new Node(point, new RectHV(0d, 0d, 1d, 1d), 0);
+            } else {
+                root.insert(point);
+            }
+            size++;
+        }
     }
 
     // does the set contain point p?
     public boolean contains(Point2D point) {
+        Node temp = root;
+        while (temp != null && temp.contains(point)) {
+            if (temp.point.equals(point)) {
+                return true;
+            } else {
+                if (temp.rt != null && temp.rt.contains(point)) {
+                    temp = temp.rt;
+                } else {
+                    temp = temp.lb;
+                }
+            }
+        }
         return false;
     }
 
@@ -36,11 +56,130 @@ public class KdTree {
 
     // all points that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
-        return new ArrayList<>();
+        List<Point2D> result = new ArrayList<>();
+        if (root != null) {
+            root.intersect(rect,result);
+        }
+        return result;
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
-    public Point2D nearest(Point2D p) {
+    public Point2D nearest(Point2D point) {
+        if (size > 0) {
+            return root.nearest(point, null);
+        }
+
         return null;
+    }
+
+    private static class Node {
+        // the point
+        private Point2D point;
+        // the axis-aligned rectangle corresponding to this node
+        private RectHV rect;
+        private int level;
+        // the left/bottom subtree
+        private Node lb;
+        // the right/top subtree
+        private Node rt;
+
+        Node(Point2D point, RectHV rectHV, int level) {
+            this.point = point;
+            this.rect = rectHV;
+            this.level = level;
+        }
+
+        void insert(Point2D toBeAdded) {
+            if (lb != null && lb.contains(toBeAdded)) {
+                lb.insert(toBeAdded);
+            } else if (rt != null && rt.contains(toBeAdded)) {
+                rt.insert(toBeAdded);
+            } else {
+                if (isVertical()) {
+                    //vertical
+                    if (Double.compare(toBeAdded.x(), point.x()) >= 0) {
+                        rt = new Node(toBeAdded, new RectHV(point.x(), rect.ymin(), rect.xmax(), rect.ymax()), level + 1);
+                    } else {
+                        lb = new Node(toBeAdded, new RectHV(rect.xmin(), rect.ymin(), point.x(), rect.ymax()), level + 1);
+                    }
+                } else {
+                    //horizontal
+                    if (Double.compare(toBeAdded.y(), this.point.y()) >= 0) {
+                        rt = new Node(toBeAdded, new RectHV(rect.xmin(), point.y(), rect.xmax(), rect.ymax()), level + 1);
+                    } else {
+                        lb = new Node(toBeAdded, new RectHV(rect.xmin(), rect.ymin(), rect.xmax(), point.y()), level + 1);
+                    }
+                }
+            }
+        }
+
+        boolean contains(Point2D point) {
+            return rect.contains(point);
+        }
+
+        void intersect(RectHV rect, List<Point2D> result) {
+            if (this.rect.intersects(rect)) {
+                if (rect.contains(point)) {
+                    result.add(point);
+                    if (rt != null) {
+                        rt.intersect(rect, result);
+                    }
+                    if (lb != null) {
+                        lb.intersect(rect, result);
+                    }
+                }
+            }
+        }
+
+        Point2D nearest(Point2D searched, Point2D nearest) {
+
+            if (!this.contains(searched) && nearest != null) {
+                double nearestDistance = searched.distanceSquaredTo(nearest);
+
+                if (isVertical() && nearestDistance < searched.distanceTo(new Point2D(this.point.x(), searched.y()))) {
+                    return nearest;
+                }
+                if (!isVertical() && nearestDistance < searched.distanceTo(new Point2D(searched.x(), this.point.y()))) {
+                    return nearest;
+                }
+
+            }
+
+            double distance = searched.distanceSquaredTo(this.point);
+            if(nearest == null || distance < searched.distanceSquaredTo(nearest)) {
+                nearest = this.point;
+            }
+
+            List<Node> toBeSearched = new LinkedList<>();
+            if (isVertical()) {
+                if (this.point.x() >= searched.x()) {
+                    toBeSearched.add(this.lb);
+                    toBeSearched.add(this.rt);
+                } else {
+                    toBeSearched.add(this.rt);
+                    toBeSearched.add(this.lb);
+                }
+            } else {
+                if (this.point.y() >= searched.y()) {
+                    toBeSearched.add(this.lb);
+                    toBeSearched.add(this.rt);
+                } else {
+                    toBeSearched.add(this.rt);
+                    toBeSearched.add(this.lb);
+                }
+            }
+
+            for (Node next : toBeSearched) {
+                if (next!=null) {
+                    nearest = next.nearest(searched, nearest);
+                }
+            }
+            return nearest;
+
+        }
+
+        private boolean isVertical() {
+            return level % 2 == 0;
+        }
     }
 }
--- test/kd-trees/KdTreeTest.groovy
+++ test/kd-trees/KdTreeTest.groovy
@@ -1,4 +1,107 @@
+import edu.princeton.cs.algs4.Point2D
+import edu.princeton.cs.algs4.RectHV
 import spock.lang.Specification
 
+import static org.hamcrest.Matchers.containsInAnyOrder
+import static spock.util.matcher.HamcrestSupport.expect
+
 class KdTreeTest extends Specification {
+
+    def 'should return true for empty set'() {
+        expect:
+        new KdTree().empty
+    }
+
+    def 'should return false for non-empty set'() {
+        given:
+        KdTree kdTree = new KdTree()
+        kdTree.insert(new Point2D(1, 1))
+
+        expect:
+        !kdTree.empty
+    }
+
+    def 'should return whether tree contains point'() {
+        given:
+        KdTree kdTree = new KdTree()
+        [new Point2D(0.5, 0.5), new Point2D(0.1, 0.1), new Point2D(0.5, 0.1), new Point2D(0.1, 0.5),
+         new Point2D(0.2, 0.4), new Point2D(0.3, 0.5)].forEach { kdTree.insert(it) }
+
+        expect:
+        kdTree.contains(point) == expected
+
+        where:
+        point                 | expected
+        new Point2D(0.5, 0.5) | true
+        new Point2D(0.1, 0.1) | true
+        new Point2D(0, 0)     | false
+        new Point2D(0.5, 0.1) | true
+        new Point2D(0.1, 0.5) | true
+        new Point2D(0.2, 0.4) | true
+        new Point2D(0.3, 0.5) | true
+    }
+
+    def 'should contain inserted points'() {
+        given:
+        KdTree kdTree = new KdTree()
+        def points = [new Point2D(0.9723019211910015, 0.33557429634170577),
+                     new Point2D(0.8013638213168075, 0.429910816828146),
+                     new Point2D(0.06293491389193273, 0.47927145727592557),
+                     new Point2D(0.40192475359071134, 0.5632286148614287),
+                     new Point2D(0.8990826383852947, 0.7491069587689919),
+                     new Point2D(0.42250925581784704, 0.1621354257752713),
+        ]
+        points.forEach { kdTree.insert(it) }
+
+        expect:
+        points.each {
+            assert kdTree.contains(it)
+        }
+    }
+
+    def 'should return points within rectangle'(List<Point2D> points, List<Point2D> expected) {
+        given:
+        KdTree kdTree = new KdTree()
+        points.forEach { kdTree.insert(it) }
+
+        when:
+        Iterable<Point2D> within = kdTree.range(new RectHV(0, 0, 0.5, 0.5))
+
+        then:
+        expect within, containsInAnyOrder(expected.toArray())
+
+        where:
+        points                                           | expected
+        []                                               | []
+        [new Point2D(2, 2)]                              | []
+        [new Point2D(-1, -1), new Point2D(1, 1)]         | []
+        [new Point2D(0, 0), new Point2D(0, 0.5),
+         new Point2D(0.5, 0), new Point2D(0.5, 0.5)]     | [new Point2D(0, 0), new Point2D(0, 0.5),
+                                                            new Point2D(0.5, 0), new Point2D(0.5, 0.5)]
+        [new Point2D(0, 0), new Point2D(0, 0.5),
+         new Point2D(0.5, 0), new Point2D(0.5, 0.5)]     | [new Point2D(0, 0), new Point2D(0, 0.5),
+                                                            new Point2D(0.5, 0), new Point2D(0.5, 0.5)]
+        [new Point2D(0.25, 0.25), new Point2D(0.1, 0.1),
+         new Point2D(0.51, 0.1), new Point2D(0.1, 0.51)] | [new Point2D(0.25, 0.25), new Point2D(0.1, 0.1)]
+    }
+
+    def 'should return nearest point'(List<Point2D> points, Point2D expected) {
+        given:
+        KdTree kdTree = new KdTree()
+        points.forEach { kdTree.insert(it) }
+
+        when:
+        Point2D nearest = kdTree.nearest(new Point2D(0.5, 0.5))
+
+        then:
+        nearest == expected
+
+        where:
+        points                                     | expected
+        []                                         | null
+        [new Point2D(1, 1)]                        | new Point2D(1d, 1d)
+        [new Point2D(0.5, 0.5), new Point2D(1, 1)] | new Point2D(0.5, 0.5)
+        [new Point2D(0.1, 0.1), new Point2D(0.2, 0.3),
+         new Point2D(0.6, 0.6)]                    | new Point2D(0.6, 0.6)
+    }
 }
